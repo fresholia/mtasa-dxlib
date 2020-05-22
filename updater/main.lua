d310793d5e179f2e574f63aa996a0c6b14921563
Enes Akıllıok <46111016+yourpalenes@users.noreply.github.com>
2020-05-22 10:56:33 +0300

    Updater

--- updater/main.lua
+++ updater/main.lua
@@ -2,9 +2,14 @@
 
 addEventHandler("onResourceStart", resourceRoot,
     function()
-        fetchRemote("https://raw.githubusercontent.com/yourpalenes/mta-dxlib/master/meta.xml",
-            function(data, err)
-                
+        fetchRemote("https://raw.githubusercontent.com/yourpalenes/mta-dxlib/master/updater/version.cfg",
+            function(data, error)
+                if error == 0 then
+                    local versionData = tostring(data) and fromJSON(tostring(data)) or false
+                    if versionData then
+                        
+                    end
+                end
             end
         )
     end
