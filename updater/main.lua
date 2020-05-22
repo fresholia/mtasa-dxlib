9c5fbb4d8d050d76f1f62b1efeb4c2811a6134ca
Enes Akıllıok <46111016+yourpalenes@users.noreply.github.com>
2020-05-22 11:04:17 +0300

    updater

--- updater/main.lua
+++ updater/main.lua
@@ -2,12 +2,24 @@
 
 addEventHandler("onResourceStart", resourceRoot,
     function()
+        local oepnVersionData = fileOpen("updater/version.cfg")
+        currentVersionData = fromJSON(fileRead(oepnVersionData, fileGetSize(oepnVersionData)))
+        currentVersion = currentVersionData.version
+        fileClose(oepnVersionData)
+
         fetchRemote("https://raw.githubusercontent.com/yourpalenes/mta-dxlib/master/updater/version.cfg",
             function(data, error)
                 if error == 0 then
-                    local versionData = tostring(data) and fromJSON(tostring(data)) or false
+                    local versionData = fromJSON(data) or false
                     if versionData then
-                        
+                        local newestVersion = versionData.version
+                        if newestVersion > currentVersion then
+                            outputDebugString("mta-dxlib > script version is outdated, there is an update")
+                            outputDebugString("mta-dxlib > curr version: "..currentVersion.." - newest version: "..newestVersion)
+                            outputDebugString("mta-dxlib > current version last update: "..currentVersionData.last_update)
+                            outputDebugString(" ")
+                            outputDebugString("mta-dxlib > type \"update mta-dxlib\" to update")
+                        end
                     end
                 end
             end
