284940162b1cd7869b4d63434ac2cfa3fbef00bf
Enes Akıllıok <46111016+yourpalenes@users.noreply.github.com>
2020-05-22 11:24:35 +0300

    updater backup

--- updater/main.lua
+++ updater/main.lua
@@ -1,11 +1,22 @@
+local openVersionFile = {}
 
+function explode(div, str)
+    if (div=='') then return false end
+    local pos,arr = 0,{}
+    for st,sp in function() return string.find(str,div,pos,true) end do
+      table.insert(arr,string.sub(str,pos,st-1))
+      pos = sp + 1
+    end
+    table.insert(arr,string.sub(str,pos))
+    return arr
+end
 
 addEventHandler("onResourceStart", resourceRoot,
     function()
-        local oepnVersionData = fileOpen("updater/version.cfg")
-        currentVersionData = fromJSON(fileRead(oepnVersionData, fileGetSize(oepnVersionData)))
+        local openVersionFile = fileOpen("updater/version.cfg")
+        currentVersionData = fromJSON(fileRead(openVersionFile, fileGetSize(openVersionFile)))
         currentVersion = currentVersionData.version
-        fileClose(oepnVersionData)
+        fileClose(openVersionFile)
 
         fetchRemote("https://raw.githubusercontent.com/yourpalenes/mta-dxlib/master/updater/version.cfg",
             function(data, error)
@@ -14,15 +25,52 @@ addEventHandler("onResourceStart", resourceRoot,
                     if versionData then
                         local newestVersion = versionData.version
                         if newestVersion > currentVersion then
-                            outputDebugString("mta-dxlib > script version is outdated, there is an update")
-                            outputDebugString("mta-dxlib > curr version: "..currentVersion.." - newest version: "..newestVersion)
-                            outputDebugString("mta-dxlib > current version last update: "..currentVersionData.last_update)
-                            outputDebugString(" ")
-                            outputDebugString("mta-dxlib > type \"update mta-dxlib\" to update")
+                            print("mta-dxlib > script version is outdated, there is an update")
+                            print("mta-dxlib > curr version: "..currentVersion.." - newest version: "..newestVersion)
+                            print("mta-dxlib > current version last update: "..currentVersionData.last_update)
+                            print(" ")
+                            print("mta-dxlib > type \"update mta-dxlib\" to update")
                         end
                     end
                 end
             end
         )
+    end
+)
+
+addCommandHandler("update",
+    function(element, cmd, var)
+        if element.type == "console" then
+            if var == "mta-dxlib" then
+                print("mta-dxlib > starting download process...")
+                print("mta-dxlib > you can find old files in \"old/\" folder")
+
+                local meta = xmlLoadFile("meta.xml")
+                local metaData = xmlNodeGetChildren(meta)
+                if metaData then
+                    for index, node in ipairs(metaData) do
+                        local fileType = xmlNodeGetName(node)
+                        local fileLocation = xmlNodeGetAttribute(node, "src")
+                        if fileType == "script" then
+                            if fileLocation:find("/") then
+                                local explodeFile = explode(fileLocation, "/")
+                                fileName = explodeFile[1]
+                            else
+                                fileName = fileLocation
+                            end
+                            if fileExists("old/"..currentVersion.."/"..fileLocation) then
+                                fileDelete("old/"..currentVersion.."/"..fileLocation)
+                            end
+                            fileCopy(fileLocation, "old/"..currentVersion.."/"..fileLocation)
+                        end
+                    end
+                end
+                xmlUnloadFile(meta)
+            elseif var == "cegui" then
+
+            elseif var == "cef" then
+
+            end
+        end
     end
 )
